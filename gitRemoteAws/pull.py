a6f26f68715fd6664bb0bcd4d5caa43a7129f421
Shadi Akiki <shadiakiki1986@gmail.com>
2019-07-22 14:12:52 +0000

    fixup

--- gitRemoteAws/pull.py
+++ gitRemoteAws/pull.py
@@ -135,7 +135,21 @@ def get_cwListMetrics(fn_dir, cloudwatch):
 def get_sns_listTopics(fn_dir, sns):
     # SNS paginator
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns.html#SNS.Paginator.ListTopics
-    get_cwCore(fn_dir, sns, 'list_topics', 'Topics')
+    PaginationConfig = {
+        'MaxResults': 3000 # FIXME <<<<<<<<<<<<<<<<<<<<<<<<<<<
+    }
+    operation_name = 'list_topics'
+    dict_key = 'Topics'
+    paginator = sns.get_paginator(operation_name)
+    for i, response in enumerate(paginator.paginate(PaginationConfig=PaginationConfig)):
+        for j, operation_i in enumerate(response[dict_key]):
+            # sys.stderr.write("%s/%s: %s\n"%(i, j, json.dumps(operation_i)))
+            #return # FIXME
+        
+            tid = operation_i['TopicArn'].split(':')[-1]
+            fn_temp = os.path.join(fn_dir, tid+'.json')
+            with open(fn_temp, 'w') as fh:
+                json.dump(operation_i, fh, default=json_serial, indent=4, sort_keys=True)
     
 import datetime as dt
 def get_cwGetMetricData(fn_dir1, cloudwatch, ec2):
