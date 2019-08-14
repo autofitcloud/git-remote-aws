e3a1257db1cc16a83ce104560e651248198f5a4e
Shadi Akiki <shadiakiki1986@gmail.com>
2019-08-14 15:52:02 +0000

    fixup

--- gitRemoteAws/utils.py
+++ gitRemoteAws/utils.py
@@ -3,7 +3,8 @@
 def mygetlocale():
   import locale
   l_av = locale.locale_alias.keys()
-  l_pref = ['c.utf8', 'en_US.utf8']
+  # l_pref = ['c.utf8', 'en_US.utf8']
+  l_pref = ['en_US.utf8', 'c.utf8'] # because locale_alias sucks (https://stackoverflow.com/questions/53320311/how-do-i-find-all-available-locales-in-python#comment93521010_53320311), put en_US first, then c
   l_ok = [x for x in l_pref if x in l_av]
   if len(l_ok)==0: return None
   return l_ok[0]
@@ -14,10 +15,10 @@ def mysetlocale():
   if li is None:
       return
   
-  import os
   #import logging
   #logger = logging.getLogger('git-remote-aws')
-
-  # logger.warning("Setting locale to %s"%li)
+  #logger.warning("Setting locale to %s"%li)
+  
+  import os
   os.environ["LC_ALL"] = li
   os.environ["LANG"]   = li
